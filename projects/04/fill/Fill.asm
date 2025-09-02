65dc00208807629fa41ee6fded5c1e8c2ce44eac
Jackson (Swimotter) Rubiano <mr.jacksonrubiano@gmail.com>
2025-09-02 14:53:50 -0400

    Complete project 04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -8,4 +8,52 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+// R0 - current screen pos in RAM
+// R1 - screen color (black: -1, white: 0)
+
+// Infinite loop
+(LOOP)
+@SCREEN // A=RAM[16384]
+D=A
+@R0 // A=RAM[0]
+M=D
+
+// Default to black
+@R1
+M=-1
+
+// Check input
+@KBD // A=RAM[24576]
+D=M
+@FILL
+D;JNE // Key not pressed
+
+// Set to white
+@R1
+M=0
+
+// Fill/clear screen
+(FILL)
+// Fetch the color to fill
+@R1
+D=M
+
+// Inc screen counter, set color
+@R0 // A=RAM[0]
+A=M // A=RAM[RAM[0]]
+M=D
+
+@R0 // A=RAM[0]
+M=M+1 // Where to update next tick
+D=M
+
+// Check if screen filled
+@KBD // A=RAM[24576]
+D=A-D
+
+//if R0==@KBD, the screen was completely filled
+@FILL
+D;JGT
+
+@LOOP
+0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,4 +7,33 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// zero the output
+@R2
+M=0
+
+// load, R0 check if 0 mult
+@R0
+D=M
+@END
+D;JLE
+
+// load, R1 check if 0 mult
+@R1
+D=M
+@END
+D;JLE
+
+(MULLOOP)
+@R1
+D=M
+@R2
+M=D+M
+@R0
+M=M-1
+D=M
+@MULLOOP
+D;JGT
+
+(END)
+@END
+0;JMP
